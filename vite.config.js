f49f2ad1ee0fef5b30e282a7fab5e33c5e7a734e
Aliano <portifolio@aliano.com.br>
2026-01-31 21:55:11 -0300

    Corrigindo base path para tirar a tela branca

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gestor/', // Isso garante que o site ache os arquivos na pasta /gestor
+  base:'/', // Isso garante que o site ache os arquivos na pasta /gestor
 })
